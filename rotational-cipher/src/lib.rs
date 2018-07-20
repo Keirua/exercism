6d1e6ab19029abc7f6e21a62979b7992568abb4f
clement <clement.camin@tsi-payment.com>
2018-07-20 11:31:19 +0200

    more idiomatic rotationnal cipher

--- rotational-cipher/src/lib.rs
+++ rotational-cipher/src/lib.rs
@@ -1,17 +1,10 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    let mut output: String = String::new();
-
-    for c in input.chars() {
-        if c >= 'a' && c <= 'z' {
-            let c2 = (((c as u8 + key as u8 - 'a' as u8) % 26) + 'a' as u8) as char;
-            output.push_str(&c2.to_string());
-        } else if c >= 'A' && c <= 'Z' {
-            let c2 = (((c as u8 + key as u8 - 'A' as u8) % 26) + 'A' as u8) as char;
-            output.push_str(&c2.to_string());
-        } else {
-            output.push_str(&c.to_string());
+    input.chars().map(|c| {
+        match c {
+            'a'...'z' => (((c as u8 + key as u8 - b'a') % 26) + b'a') as char,
+            'A'...'Z' => (((c as u8 + key as u8 - b'A') % 26) + b'A') as char,
+            _ => c
         }
-    }
-
-    output
+    })
+    .collect()
 }
