b2424354aae02a63cff50bb41c4ca464e29d7374
clemkeirua <clement@keiruaprod.fr>
2019-06-07 09:37:17 +0200

    solved clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -7,18 +7,30 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock {nb_minutes: 60 * hours + minutes }
+        let mut increment = 0;
+        if  60 * hours + minutes < 0{
+            while 60 * hours + minutes + 1440 * increment < 0 {
+                increment += 1;
+            }
+        }
+        if 60 * hours + minutes >= 1440 {
+            while 60 * hours + minutes + 1440 * increment >= 1440 {
+                increment -= 1;
+            }
+        }
+        Clock {nb_minutes: 60 * hours + minutes + 1440 * increment}
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock{
-            nb_minutes: self.nb_minutes + minutes
-        }
+        Clock::new(0,self.nb_minutes + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.nb_minutes)
+        let hours = (self.nb_minutes / 60) % 24;
+        let minutes = self.nb_minutes % 60;
+
+        write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
